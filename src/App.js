541c9bf3a53b2cafbdff7fbf12c1945c2cb84bb7
aadilsarfaraz30 <aadilk970@gmail.com>
2022-03-31 23:36:23 +0530

    added styled

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React $ deploy learn install react
+          Learn React $ deploy learn install react added more thing
         </a>
       </header>
     </div>
