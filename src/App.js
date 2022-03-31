06f939806a4aa76afda0af8e604e809df0f5ff70
aadilsarfaraz30 <aadilk970@gmail.com>
2022-03-31 23:20:55 +0530

    install dependcy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React $ deploy learn
+          Learn React $ deploy learn install react
         </a>
       </header>
     </div>
