37365f7c31f5d43f428c54ad0a4a744579edb300
aadilsarfaraz30 <aadilk970@gmail.com>
2022-04-11 23:47:49 +0530

    added condition in displayContainer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
-import SearchContainer from "./container/SearchContainer";
 import AppBarContainer from "./container/AppBarContainer";
 import { Stack, ThemeProvider } from "@mui/material";
 import DisplayContainer from "./container/DisplayContainer";
 import { theme } from "./styles/style";
-import { useSelector } from "react-redux";
-import LoadingComponent from "./component/LoadingComponent";
 
 function App() {
-  const loading = useSelector((state) => state.weatherData.loading);
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
